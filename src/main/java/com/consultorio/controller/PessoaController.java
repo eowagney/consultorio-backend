b8e3e5794828bf6f8005f70a3662883dcd3b8834
eowagney <wagnergomes9a@gmail.com>
2025-09-01 00:16:22 -0300

    Atualizado com editar e excluir

--- src/main/java/com/consultorio/controller/PessoaController.java
+++ src/main/java/com/consultorio/controller/PessoaController.java
@@ -2,9 +2,13 @@ package com.consultorio.controller;
 
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,4 +36,28 @@ public class PessoaController {
     public Pessoa salvar(@RequestBody Pessoa pessoa) {
         return repository.save(pessoa);
     }
+
+  @PutMapping("/{id}")
+public ResponseEntity<Pessoa> atualizar(@PathVariable Long id, @RequestBody Pessoa pessoaAtualizada) {
+    return repository.findById(id)
+        .map(pessoa -> {
+            pessoa.setNome(pessoaAtualizada.getNome());
+            pessoa.setIdade(pessoaAtualizada.getIdade());
+            pessoa.setEmail(pessoaAtualizada.getEmail());
+            Pessoa salvo = repository.save(pessoa);
+            return ResponseEntity.ok(salvo);
+        })
+        .orElse(ResponseEntity.notFound().build());
+}
+
+@DeleteMapping("/{id}")
+public ResponseEntity<Void> deletar(@PathVariable Long id) {
+    return repository.findById(id)
+        .map(pessoa -> {
+            repository.delete(pessoa);
+            return ResponseEntity.noContent().<Void>build();
+        })
+        .orElse(ResponseEntity.notFound().build());
+}
+
 }
--- src/main/java/com/consultorio/repository/PessoaRepository.java
+++ src/main/java/com/consultorio/repository/PessoaRepository.java
@@ -1,8 +1,11 @@
 package com.consultorio.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import com.consultorio.model.Pessoa;
 
 public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
+
+    Pessoa save(@RequestBody Pessoa pessoa);
 }
